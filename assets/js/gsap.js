9451da65fbe49e41aae7f32f33c0d059c1350fa5
SyedKashanAbbasNaqvi <abbaskashan234@gmail.com>
2025-08-03 03:42:32 +0500

    `make sponsor section: update CSS and HTML, add Swiper carousel functionality`

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -71,33 +71,8 @@ $("#closeNav").on("click", function () {
   });
 });
 
-// === Swiper Carousel ===
-// Initialize Swiper
-const swiper = new Swiper('.testimonials-slider', {
-    loop: true,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-        },
-    },
-});
+
+
 
 
 // === Parallax Scroll Animations ===
---
+++ assets/js/swiper.js
@@ -0,0 +1,23 @@
+ const swiper = new Swiper('.sponsor-swiper', {
+    loop: true,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 2,
+      }
+    }
+  });
